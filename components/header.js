72a27d14d8c9884b7073b7e96df9a682b7ad89c0
Adrian de Souza <adriandesouza1@gmail.com>
2026-01-13 11:17:51 +0200

    Fix home page assets

--- components/header.js
+++ components/header.js
@@ -2,8 +2,8 @@ export default function Header() {
   return `
     <header class="main-header">
       <div class="header-container">
-        <a href="/" class="logo" aria-label="Home">
-          <img src="/public/img/logo-header.png" alt="EduGate" />
+        <a href="./pages/home.html" class="logo" aria-label="Home">
+          <img src="../public/img/logo-header.png" alt="EduGate" />
         </a>
 
         <button id="burger-btn" class="burger-btn"
@@ -14,14 +14,14 @@ export default function Header() {
         <nav id="header-nav" class="header-nav" role="navigation" aria-hidden="true">
           <button id="close-nav" class="close-nav" aria-label="Close menu">✕</button>
 
-          <a href="/pages/about.html">
+          <a href="./pages/about.html">
             <span>About</span>
-            <img src="/public/icons/about.png" class="nav-icon" alt="About icon" />
+            <img src="../public/icons/about.png" class="nav-icon" alt="About icon" />
           </a>
 
-          <a href="/pages/contact.html">
+          <a href="./pages/contact.html">
             <span>Contact</span>
-            <img src="/public/icons/contact.png" class="nav-icon" alt="Contact icon" />
+            <img src="../public/icons/contact.png" class="nav-icon" alt="Contact icon" />
           </a>
         </nav>
       </div>
